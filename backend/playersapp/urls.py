163fe6338a00cb39fba87659e0751a007e8d4c74
GuilhermeLopes06 <guilhermedemoraes54@gmail.com>
2025-05-05 16:54:23 -0300

    Incremento para apresentação

--- backend/playersapp/urls.py
+++ backend/playersapp/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('cadastrar/', views.cadastrar_funcionario, name='cadastrar_funcionario'),
     path('sucesso/', views.funcionario_sucesso, name='funcionario_sucesso'),
-    path('funcionarios/', views.listar_funcionarios, name='listar_funcionarios'),  # Nova rota para listar funcionários
+    path('funcionarios/', views.listar_funcionarios, name='listar_funcionarios'),  # Rota para listar funcionários
 ]
