8ad2397fd2b98eb71d06a7598f60d6d38a8f760c
nicholas-vasconcelos <nicholasbvasconcelos@gmail.com>
2025-06-02 16:43:01 -0300

    final edit

--- backend/playersapp/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class PlayersappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'playersapp'
-
-    def ready(self):
-        import playersapp.signals  # noqa
--- backend/playersapp/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/projects/$', consumers.ProjectConsumer.as_asgi()),
-    re_path(r'ws/teams/$', consumers.TeamConsumer.as_asgi()),
-]
